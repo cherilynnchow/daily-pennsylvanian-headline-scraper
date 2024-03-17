3804793a6f72f3b3554cbf7fe92c96b8abdbc8d0
cherilynnchow <60800189+cherilynnchow@users.noreply.github.com>
2024-03-17 00:10:12 -0400

    Update script.py to find most read article

--- script.py
+++ script.py
@@ -26,8 +26,7 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        div = soup.find('div', class_="most-read-item")
-        target_element = div.find("a", class_="frontpage-link standard-link")
+        target_element = soup.find("a", class_="frontpage-link standard-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
